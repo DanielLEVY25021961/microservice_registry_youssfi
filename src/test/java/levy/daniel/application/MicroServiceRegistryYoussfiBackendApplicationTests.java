639d5d433c579107edc7164e917ceb6044853ef5
DanielLEVY25021961 <dany.levy@free.fr>
2020-10-13 14:03:30 +0200

    COMMIT_ORIGINEL

---
+++ src/test/java/levy/daniel/application/MicroServiceRegistryYoussfiBackendApplicationTests.java
@@ -0,0 +1,49 @@
+package levy.daniel.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * CLASSE MicroServiceRegistryYoussfiBackendApplicationTests :<br/>
+ * .<br/>
+ * <br/>
+ *
+ * - Exemple d'utilisation :<br/>
+ *<br/>
+ * 
+ * - Mots-clé :<br/>
+ * <br/>
+ *
+ * - Dépendances :<br/>
+ * <br/>
+ *
+ *
+ * @author Daniel Lévy
+ * @version 1.0
+ * @since 4 oct. 2020
+ */
+@SpringBootTest
+public class MicroServiceRegistryYoussfiBackendApplicationTests {
+
+	
+	
+	/**
+	 * CONSTRUCTEUR D'ARITE NULLE.
+	 */
+	public MicroServiceRegistryYoussfiBackendApplicationTests() {
+		super();
+	} // Fin du CONSTRUCTEUR D'ARITE NULLE.________________________________
+	
+	
+
+	/**
+	 * 
+	 */
+	@Test
+	void contextLoads() {
+		/**/
+	} // Fin de contextLoads().____________________________________________
+	
+	
+
+} // FIN DE LA CLASSE MicroServiceRegistryYoussfiBackendApplicationTests.----
